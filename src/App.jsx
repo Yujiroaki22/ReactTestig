162901924caa1f3f684a5372a0606d0afdc4b6e6
jake <zarniheinzaz@gmail.com>
2025-03-12 19:43:49 +0630

    Usestate-Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,11 @@
 
-import Button from './Button.jsx'
-import Profile from './Profile.jsx'
+import Counter from "./Counter.jsx";
 function App() {
 
-  /*const fruits=  [{id: 1,name :"apple",calories: 105},
-    {id: 2,name:"orange",calories: 100},
-    {id: 3,name:"grape", calories: 45},
-    {id: 4,name:"banana",calories: 55}];
-
-    const vegetable=  [{id: 5,name :"carrots",calories: 35},
-                    {id: 6,name:"potatoes",calories: 120},
-                    {id: 7,name:"toamato", calories: 15},
-                    {id: 8,name:"corn",calories: 75}];
-                    */
+  
   return (
   <>
-  <Profile/>
+  <Counter/>
 
     </>
   );
---
+++ src/Mycomponent.jsx
@@ -0,0 +1,30 @@
+import React ,{useState, usestate} from 'react';
+
+function Mycomponent(){
+    let [name, setName] = useState("Guest"); 
+    const [age,setAge]=useState(0);
+    const [isEmployed,setisEmployed]=useState(false);
+
+    const updateName= () => {
+        setName("Jake");
+    }   
+    const incrementAge= () =>{
+        setAge(age+1);
+    }
+    const toggleEmployedStatus=() =>{
+        setisEmployed(!isEmployed);
+    }
+
+
+    return(<div>
+        <p>Name: {name}</p>
+        <button onClick={updateName}>Set Name</button>
+        <p>Age: {age}</p>
+        <button onClick={incrementAge}>Increment Age</button>
+        <p>Is employed: {isEmployed ? "Yes" : "No"}</p>
+        <button onClick={toggleEmployedStatus}>Toggle Status</button>
+    </div>);
+
+
+}
+export default Mycomponent
